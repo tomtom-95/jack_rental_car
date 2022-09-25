ed3b624b69b997409b0aa00d7d1eac52d4dd89f6
Tommaso Pellegrino <tommasopellegrino.1995@gmail.com>
2022-09-25 12:07:16 +0200

    using vectors

--- src/jack_rental_car.cpp
+++ src/jack_rental_car.cpp
@@ -1,103 +1,61 @@
 #include <iostream>
+#include <vector>
 
-int cars_num = 5;
+int cars_num = 10;
 
-double dynamics(int next_a_cars, int next_b_cars, int reward, int prev_a_cars, int prev_b_cars, int action);
-double update_state_value(int a_cars, int b_cars, int policy[cars_num][cars_num], double state_value[cars_num][cars_num]);
-int argmax(int a_cars, int b_cars, double state_value[cars_num][cars_num]);
+double dynamics(int a_cars_nxt, int b_cars_nxt, int reward, int a_cars_prev, int b_cars_prev, int action);
+double update_state_values(int a_cars, int b_cars, std::vector<std::vector<double>> state_values, std::vector<std::vector<int>> policy);
 
-// p(s', r | s, a)
-//TODO: problem with my implementation (I think due to the policy definition) I do not explore other states and I find as best value function
-double dynamics(int next_a_cars, int next_b_cars, int reward, int prev_a_cars, int prev_b_cars, int action) {
-  int a_cars_taken = std::min(prev_a_cars - action + 2, 3);
-  int b_cars_taken = std::min(prev_b_cars + action + 3, 4);
+// dynamics for totally deterministic case
+double dynamics(int a_cars_nxt, int b_cars_nxt, int reward, int a_cars_prev, int b_cars_prev, int action) {
+  int a_cars_taken = std::min(a_cars_prev - action + 2, 3);
+  int b_cars_taken = std::min(b_cars_prev + action + 3, 4);
+
+  int a_cars = std::min(a_cars_prev - a_cars_taken + 2, cars_num);
+  int b_cars = std::min(b_cars_prev - b_cars_taken + 3, cars_num);
 
   int real_reward = 10 * a_cars_taken + 10 * b_cars_taken - 2 * abs(action);
 
-  if ((next_a_cars == prev_a_cars - a_cars_taken + 2) &&
-      (next_b_cars == prev_b_cars - b_cars_taken + 3) &&
-      (reward == real_reward)) {
-    return 1;
-  }
-  else {
-    return 0;
-  }
+  return (a_cars == a_cars_nxt) && (b_cars == b_cars_nxt) && (real_reward == reward);
 }
 
-double update_state_value(int a_cars, int b_cars, int policy[cars_num][cars_num], double state_value[cars_num][cars_num]) {
+double update_state_values(int a_cars, int b_cars, std::vector<std::vector<double>> state_values, std::vector<std::vector<int>> policy) {
   double new_value = 0;
+
   for (int i = 0; i != cars_num; ++i) {
     for (int j = 0; j != cars_num; ++j) {
-      for (int reward = -10; reward != 70; ++reward) {
-        new_value += dynamics(i, j, reward, a_cars, b_cars, policy[a_cars][b_cars]) * (reward + 0.9 * state_value[i][j]);
+      for (int r = -10; r != 71; ++r) {
+        new_value += dynamics(i, j, r, a_cars, b_cars, policy[i][j]) * (r + 0.9 * state_values[i][j]);
       }
     }
   }
+
   return new_value;
 }
 
-int argmax(int a_cars, int b_cars, double state_value[cars_num][cars_num]) {
-  double value = 0;
-  double max_value = 0;
-  int max_action = 0;
-  for (int a = -5 ; a != 6; ++a) {
-    for (int i = 0; i != cars_num; ++i) {
-      for (int j = 0; j != cars_num; ++j) {
-        for (int reward = -10; reward != 70; ++reward) {
-          value = dynamics(i, j, reward, a_cars, b_cars, a) * (reward + 0.9 * state_value[i][j]);
-        }
-      }
-    }
-    if (max_value < value) {
-      max_value = value;
-      max_action = a;
-    }
-  }
-  return max_action; 
-}
-
 int main(int argc, char *argv[]) {
-  double state_value[cars_num][cars_num] = {};
-  int policy[cars_num][cars_num] = {};
+  std::vector<std::vector<double>> state_values(cars_num, std::vector<double>(cars_num));
+  std::vector<std::vector<int>> policy(cars_num, std::vector<int>(cars_num));
 
-  bool policy_stable = false;
-  while (policy_stable == false) {
-    // policy evaluation
-    double delta = 10;
-    double theta = 1;
-    while (delta > theta) {
-      delta = 0;
-      for (int i = 0; i != cars_num; ++i) {
-        for (int j = 0; j != cars_num; ++j) {
-          double temp_value = state_value[i][j];
-          state_value[i][j] = update_state_value(i, j, policy, state_value);
-          std::cout << state_value[i][j] << std::endl;
-          delta = std::max(delta, abs(temp_value - state_value[i][j]));
-        }
-      }
-    }
-
-    return 0;
-
-    // policy improvement
-    policy_stable = true;
+  // policy evaluation
+  double delta = 100;
+  double theta = 1;
+  while (delta > theta) {
+    delta = 0;
     for (int i = 0; i != cars_num; ++i) {
       for (int j = 0; j != cars_num; ++j) {
-        int old_action = policy[i][j];
-        policy[i][j] = argmax(i, j, state_value);
-        if (old_action != policy[i][j]) {
-          policy_stable = false;
-        }
+        double temp_value = state_values[i][j];
+        state_values[i][j] = update_state_values(i, j, state_values, policy);
+        delta = std::max(delta, abs(temp_value - state_values[i][j]));
       }
     }
   }
 
-  for (int i = 0; i != cars_num; ++i) {
-    for (int j = 0; j != cars_num; ++j) {
-      std::cout << "state_value[" << i << "][" << j << "] = " << state_value[i][j] << std::endl;
-      std::cout << "policy[" << i << "][" << j << "] = " << policy[i][j] << std::endl;
+  for (auto i: state_values) {
+    for (auto j: i) {
+      std::cout << j << std::endl;
     }
   }
-  
+
   return 0;
 }
